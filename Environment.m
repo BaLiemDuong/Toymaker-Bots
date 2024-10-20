0de733127956ed01bd219e5c26a1d17e7d9955a2
BaLiemDuong <baliem.duong@student.uts.edu.au>
2024-10-21 04:48:21 +1100

    Update Environment.m
    added a variety of objects, environment close to finalizing

--- Environment.m
+++ Environment.m
@@ -2,35 +2,83 @@ classdef Environment < handle
     properties
     end
     methods (Static)
-        % function place_fence(x,y,z, rotz)
-        %     fence = PlaceObject('barrier1.5x0.2x1m.ply',[x,y,z]);
-        %     verts = [get(fence,'Vertices'),ones(size(get(fence,'Vertices'),1),1)]*trotz(rotz);
-        %     verts(:,2) = verts(:,2)*(5/3);
-        %     set(fence,'Vertices',verts(:,1:3));
-        % end
-        % function place_stationary_objects(table, button, belt1, belt2)
-        %     PlaceObject('tableBlue1x1x0.5m.ply',table);
-        %     PlaceObject('emergencyStopButton.ply',button);
-        %     PlaceObject('beltconverter.PLY',belt1);
-        %     PlaceObject('beltconverter.PLY',belt2);
-        % end
-        % function car_parts(c1)%, c2, c3, w1, w2, w3, w4)
-        %     PlaceObject('car_base.ply',c1);
-        % end
-
-        % function place_object(name, x,y,z, rotx,roty,rotz)
+    %% Function to place floor texture
+        function floor()
+            surf([-2,-2;2,2],[-2.5,1;-2.5,1],[0,0;0,0], ...
+                'CData',imread('tilefloor.jpg'),'FaceColor','texturemap');
+        end
+    %% Functions to place objects
+        % function place_object(name, x,y,z)
         %     object = PlaceObject(name,[x,y,z]);
-        %     verts = [get(object,'Vertices'),ones(size(get(object,'Vertices'),1),1)]*trotz(rotx)*trotz(roty)*trotz(rotz);
-        %     verts(:,2) = verts(:,2)*(5/3);
-        %     set(object,'Vertices',verts(:,1:3));
+        %     vertices = get(object,'Vertices');
+        %     transformedVertices = [vertices,ones(size(vertices,1),1)];
+        %     set(object,'Vertices',transformedVertices(:,1:3));
         % end
-
-        function place_object(name, x,y,z, rotx,roty,rotz)
-            object = PlaceObject(name,[x,y,z]);
+        function fence1(x1,y1,z1)
+            object = PlaceObject('barrier1.5x0.2x1m.ply',[x1,y1,z1]);
             vertices = get(object,'Vertices');
-            transformedVertices = [vertices,ones(size(vertices,1),1)]*trotz(rotx)*trotz(roty)*trotz(rotz);
+            transformedVertices = [vertices,ones(size(vertices,1),1)]*trotz(pi/2);
             set(object,'Vertices',transformedVertices(:,1:3));
         end
-
+        function fence2(x2,y2,z2)
+            object = PlaceObject('barrier1.5x0.2x1m.ply',[x2,y2,z2]);
+            vertices = get(object,'Vertices');
+            transformedVertices = [vertices,ones(size(vertices,1),1)]*trotz(-pi/2);
+            set(object,'Vertices',transformedVertices(:,1:3));
+        end
+        function fence3(x3,y3,z3)
+            object = PlaceObject('barrier1.5x0.2x1m.ply',[x3,y3,z3]);
+            vertices = get(object,'Vertices');
+            transformedVertices = [vertices,ones(size(vertices,1),1)]*trotz(deg2rad(-30));
+            set(object,'Vertices',transformedVertices(:,1:3));
+        end
+        function fence4(x4,y4,z4)
+            object = PlaceObject('barrier1.5x0.2x1m.ply',[x4,y4,z4]);
+            vertices = get(object,'Vertices');
+            transformedVertices = [vertices,ones(size(vertices,1),1)]*trotz(deg2rad(30));
+            set(object,'Vertices',transformedVertices(:,1:3));
+        end
+        function conveyer1(x5,y5,z5)
+            object = PlaceObject('beltconverter.ply',[x5,y5,z5]);
+            vertices = get(object,'Vertices');
+            transformedVertices = [vertices,ones(size(vertices,1),1)];
+            set(object,'Vertices',transformedVertices(:,1:3));
+        end
+        function conveyer2(x6,y6,z6)
+            object = PlaceObject('beltconverter.ply',[x6,y6,z6]);
+            vertices = get(object,'Vertices');
+            transformedVertices = [vertices,ones(size(vertices,1),1)];
+            set(object,'Vertices',transformedVertices(:,1:3));
+        end
+        function table(x7,y7,z7)
+            object = PlaceObject('tableBlue1x1x0.5m.ply',[x7,y7,z7]);
+            vertices = get(object,'Vertices');
+            transformedVertices = [vertices,ones(size(vertices,1),1)];
+            set(object,'Vertices',transformedVertices(:,1:3));
+        end
+        function button(x8,y8,z8)
+            object = PlaceObject('emergencyStopButton.ply',[x8,y8,z8]);
+            vertices = get(object,'Vertices');
+            transformedVertices = [vertices,ones(size(vertices,1),1)];
+            set(object,'Vertices',transformedVertices(:,1:3));
+        end
+        function car_base(x9,y9,z9)
+            object = PlaceObject('car_base.ply',[x9,y9,z9]);
+            vertices = get(object,'Vertices');
+            transformedVertices = [vertices,ones(size(vertices,1),1)];
+            set(object,'Vertices',transformedVertices(:,1:3));
+        end
+        function car_body(x10,y10,z10)
+            object = PlaceObject('car_body.ply',[x10,y10,z10]);
+            vertices = get(object,'Vertices');
+            transformedVertices = [vertices,ones(size(vertices,1),1)];
+            set(object,'Vertices',transformedVertices(:,1:3));
+        end
+        function wheels(x11,y11,z11)
+            object = PlaceObject('wheels_axial.ply',[x11,y11,z11]);
+            vertices = get(object,'Vertices');
+            transformedVertices = [vertices,ones(size(vertices,1),1)];
+            set(object,'Vertices',transformedVertices(:,1:3));
+        end
     end
 end
