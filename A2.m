07ef0985667ba6e36b860d9c78c0d714aee2653a
BaLiemDuong <baliem.duong@student.uts.edu.au>
2024-10-21 04:49:59 +1100

    Update A2.m
    Move the operating functions to a different file, write lines to place object

--- A2.m
+++ A2.m
@@ -2,94 +2,58 @@ clc;
 clf;
 hold on;
 
-axis ([-1 1 -1 1 -0.1 1.3]);
-r = HansCute(transl(0,0,0.555));
-
+% Set figure size
+axis ([-2 2 -2.5 1 -0.1 1.5]);
+% Place Hans bot
+r = HansCute(transl(-0.4,0,0.555));
 % Take the base of end-effector 
-% qNow = r.model.getpos();
-% base = r.model.fkineUTS(qNow);
 base = r.model.fkineUTS(r.model.getpos());
-%Call the gripper with 2 fingers and plot the calculated base 
-finger1 = HansFinger(base*transl(-0.016,-0.03,0.08)*trotz(deg2rad(35)));%(base*transl(0, 0.02, 0));%(base*trotx(pi/2));
-finger2 = HansFinger(base*transl(-0.016,0.03,0.08)*trotz(deg2rad(215)));%*transl(0, -0.02, 0)*trotz(pi)); %base*troty(pi)*trotx(-pi/2)
+%Call the 2 fingers and plot the calculated base 
+finger1 = HansFinger(base*transl(-0.016,-0.03,0.08)*trotz(deg2rad(35)));
+finger2 = HansFinger(base*transl(-0.016,0.03,0.08)*trotz(deg2rad(215)));
+%% Position of objects
+% Fence 1
+x1 = 0;y1 = 1.3;z1 = 0;
+% Fence 2
+x2 = 0;y2 = 1.3;z2 = 0;
+% Fence 3
+x3 = 0;y3 = -1.3;z3 = 0;
+% Fence 4
+x4 = 0;y4 = -1.3;z4 = 0;
+% Conveyer 1
+x5 = -0.8;y5 = 0;z5 = 0;
+% Conveyer 2
+x6 = 0.9;y6 = 0;z6 = 0;
+% Table
+x7 = 0;y7 = 0;z7 = 0;
+% E-stop
+x8 = 0.4;y8 = -0.3;z8 = 0.5;
+% Car body
+x9 = 0.8;y9 = 0;z9 = 0.57;
+% Car
+x10 = -0.8;y10 = 0.1;z10 = 0.57;
+% Car
+x11 = -0.8;y11 = -0.1;z11 = 0.57;
 
-%PlaceObject('tableBlue1x1x0.5m.ply', [0,0,0]);
-%Environment.place_fence(0.5,0.5,0,pi/2);
-%Environment.place_stationary_objects([0,0,0],[0.3,0.3,0.55],[-0.5,0.5,0],[0.5,-0.5,0]);
-%Environment.place_object('carbase.ply',0.2,0.2,0.6,0,0,0);
-
-% q0 = [0,0,0,0,0,0];
-% q1 = [-0.016,-0.03,0.08];
-% q2 = [-0.8,-0.8,0.08];
-% tr = jtraj(q1,q2,50);
-% finger1.model.animate(tr(50,:));
-%% 
-
-function qtrajec = CreateTraj(robot,brickPosition,jointGuess)
-    steps = 100;
-    qNow = robot.model.getpos();
-    T = transl(brickPosition)*trotx(pi)*troty(0)*trotz(0);
-    %Joint states that pick up the brick
-    qMove = robot.model.ikcon(T);  
-    qtrajec = jtraj(qNow,qMove,steps);
-end
-
-function move(r, qTraj,finger1,finger2)
-    for i = 1:size(qTraj,1)
-        q = qTraj(i,:);
-        r.model.animate(q);
-        MoveFinger(r,q,finger1,finger2,i);
-        pause(0.01)
-    end
-end
-
-function MoveFinger(r,q,finger1,finger2,i)
-
-            q_f1 = finger1.model.getpos();
-            q_f2 = finger2.model.getpos();
-            q_f1_end = deg2rad([25 0]);
-            q_f2_end = deg2rad([25 0]);
-            q_f1_traj = jtraj(q_f1,q_f1_end,100);
-            q_f2_traj = jtraj(q_f2,q_f2_end,100);
-            base = r.model.fkineUTS(q);
-            
-            %Update the base of the gripper and also move the finger
-            %simutaneously
-            finger1.model.base=base*trotx(pi/2);
-
-            finger1.model.animate(q_f1_traj(i,:));
-
-            finger2.model.base=base*troty(pi)*trotx(-pi/2);
-
-            finger2.model.animate(q_f2_traj(i,:));
-
-        end
-
-function MoveFinger2(r,q,finger1,finger2,i)
-
-            % q_f1 = finger1.model.getpos();
-            % q_f2 = finger2.model.getpos();
-            % q_f1_end = deg2rad([25 0]);
-            % q_f2_end = deg2rad([25 0]);
-            % q_f1_traj = jtraj(q_f1,q_f1_end,100);
-            % q_f2_traj = jtraj(q_f2,q_f2_end,100);
-            q_f1_traj = transl(-pi/2,0,0);
-            q_f2_traj = transl(pi/2,0,0);
-            base = r.model.fkineUTS(q);
-            
-            %Update the base of the gripper and also move the finger
-            %simutaneously
-            finger1.model.base=base*trotx(pi/2);
-
-            finger1.model.animate(q_f1_traj);%(i,:)
-
-            finger2.model.base=base*troty(pi)*trotx(-pi/2);
-
-            finger2.model.animate(q_f2_traj);%(i,:)
-
-end
-
-hold off;
+% Place objects
+Environment.floor();
+Environment.fence1(x1,y1,z1);
+Environment.fence2(x2,y2,z2);
+Environment.fence3(x3,y3,z3);
+Environment.fence4(x4,y4,z4);
+Environment.conveyer1(x5,y5,z5);
+Environment.conveyer2(x6,y6,z6);
+Environment.table(x7,y7,z7);
+Environment.button(x8,y8,z8);
+Environment.car_base(x9,y9,z9);
+Environment.car_body(x10,y10,z10);
+Environment.wheels(x11,y11,z11);
+daspect([1 1 1]);
+% Add axis labels
+xlabel('X-axis');
+ylabel('Y-axis');
+zlabel('Z-axis');
+%% Move and stuffs
 qTraj = CreateTraj(r, [0.25,0.25,0.75], q0);
 move(r, qTraj,finger1,finger2);
 
