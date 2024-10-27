9a4aad13c94b3a5981d5aa2421c4af204ce52570
BaLiemDuong <baliem.duong@student.uts.edu.au>
2024-10-27 18:05:22 +1100

    Update ToyBot.m

--- @ToyBot/ToyBot.m
+++ @ToyBot/ToyBot.m
@@ -1,4 +1,5 @@
 classdef ToyBot < RobotBaseClass
+classdef ToyBot < RobotBaseClass
 
     properties(Access = public)   
         plyFileNameStem = 'ToyBot';
@@ -11,21 +12,21 @@ classdef ToyBot < RobotBaseClass
             if nargin < 1			
 				baseTr = transl(0,0,0);				
             end
-            self.model.base =  baseTr  ;%self.model.base.T * 
+            self.model.base =  baseTr;
             
             self.PlotAndColourRobot();        
         end
 
         %% Create the robot model
         function CreateModel(self)
-            link(0) = Link('d',0,'a',0,'alpha',0,'qlim',deg2rad([0 0]),'offset',deg2rad(30));
             link(1) = Link('d',0,'a',0,'alpha',0,'qlim',deg2rad([0 0]),'offset',deg2rad(0)); 
-            link(2) = Link('d',0.15,'a',0,'alpha',0,'qlim',deg2rad([0 0]),'offset',deg2rad(30));
-            link(3) = Link('d',0.15,'a',0.1,'alpha',pi/2,'qlim',deg2rad([0 0]),'offset',deg2rad(30)); 
-            link(4) = Link('d',0.3,'a',0.1,'alpha',0,'qlim',deg2rad([0 0]),'offset',deg2rad(30));
-            link(5) = Link('d',0,'a',0,'alpha',0,'qlim',deg2rad([0 0]),'offset',deg2rad(30)); 
-            link(6) = Link('d',0,'a',0,'alpha',0,'qlim',deg2rad([0 0]),'offset',deg2rad(30));
-            link(7) = Link('d',0,'a',0,'alpha',0,'qlim',deg2rad([0 0]),'offset',deg2rad(30));
+            link(2) = Link('d',0,'a',0,'alpha',0,'qlim',deg2rad([0 0]),'offset',deg2rad(0));
+            link(3) = Link('d',0,'a',0,'alpha',0,'qlim',deg2rad([0 0]),'offset',deg2rad(0)); 
+            link(4) = Link('d',0,'a',0,'alpha',0,'qlim',deg2rad([0 0]),'offset',deg2rad(0));
+            link(5) = Link('d',0,'a',0,'alpha',0,'qlim',deg2rad([0 0]),'offset',deg2rad(0)); 
+            link(6) = Link('d',0,'a',0,'alpha',0,'qlim',deg2rad([0 0]),'offset',deg2rad(0));
+            link(7) = Link('d',0,'a',0,'alpha',0,'qlim',deg2rad([0 0]),'offset',deg2rad(0));
+            link(8) = Link('d',0,'a',0,'alpha',0,'qlim',deg2rad([0 0]),'offset',deg2rad(0));
 
             self.model = SerialLink(link,'name',self.name);
             self.model.plotopt = {'noshadow','noarrow','noshading','nowrist','nojaxes'};
