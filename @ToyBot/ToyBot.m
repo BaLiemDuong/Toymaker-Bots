01d7789bea7e5558fb9d0134c972c0a51b64104e
BaLiemDuong <baliem.duong@student.uts.edu.au>
2024-10-29 16:02:47 +1100

    Add files via upload

--- @ToyBot/ToyBot.m
+++ @ToyBot/ToyBot.m
@@ -1,5 +1,4 @@
 classdef ToyBot < RobotBaseClass
-classdef ToyBot < RobotBaseClass
 
     properties(Access = public)   
         plyFileNameStem = 'ToyBot';
@@ -19,14 +18,13 @@ classdef ToyBot < RobotBaseClass
 
         %% Create the robot model
         function CreateModel(self)
-            link(1) = Link('d',0,'a',0,'alpha',0,'qlim',deg2rad([0 0]),'offset',deg2rad(0)); 
-            link(2) = Link('d',0,'a',0,'alpha',0,'qlim',deg2rad([0 0]),'offset',deg2rad(0));
-            link(3) = Link('d',0,'a',0,'alpha',0,'qlim',deg2rad([0 0]),'offset',deg2rad(0)); 
-            link(4) = Link('d',0,'a',0,'alpha',0,'qlim',deg2rad([0 0]),'offset',deg2rad(0));
-            link(5) = Link('d',0,'a',0,'alpha',0,'qlim',deg2rad([0 0]),'offset',deg2rad(0)); 
-            link(6) = Link('d',0,'a',0,'alpha',0,'qlim',deg2rad([0 0]),'offset',deg2rad(0));
-            link(7) = Link('d',0,'a',0,'alpha',0,'qlim',deg2rad([0 0]),'offset',deg2rad(0));
-            link(8) = Link('d',0,'a',0,'alpha',0,'qlim',deg2rad([0 0]),'offset',deg2rad(0));
+            link(1) = Link('d',0,'a',0,'alpha',0,'qlim',deg2rad([-45 45]),'offset',deg2rad(0)); 
+            link(2) = Link('d',0.1,'a',0,'alpha',0,'qlim',deg2rad([-90 90]),'offset',deg2rad(0));
+            link(3) = Link('d',0,'a',0.005,'alpha',pi/2,'qlim',deg2rad([-90 90]),'offset',deg2rad(0)); 
+            link(4) = Link('d',-0.05,'a',0.1,'alpha',-pi/2,'qlim',deg2rad([-90 90]),'offset',deg2rad(0));
+            link(5) = Link('d',0.1,'a',-0.02,'alpha',0,'qlim',deg2rad([0 0]),'offset',deg2rad(0)); 
+            link(6) = Link('d',-0.02,'a',0.02,'alpha',0,'qlim',deg2rad([0 0]),'offset',deg2rad(0));
+            link(7) = Link('d',0.05,'a',-0.015,'alpha',0,'qlim',deg2rad([0 0]),'offset',deg2rad(0));
 
             self.model = SerialLink(link,'name',self.name);
             self.model.plotopt = {'noshadow','noarrow','noshading','nowrist','nojaxes'};
