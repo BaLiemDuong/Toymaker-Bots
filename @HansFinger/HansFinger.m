d6892b7f9c44e69ed6dac5b3e6797a87605a5780
BaLiemDuong <baliem.duong@student.uts.edu.au>
2024-11-06 14:00:54 +1100

    Update HansFinger.m
    rewrote link parameters

--- @HansFinger/HansFinger.m
+++ @HansFinger/HansFinger.m
@@ -18,14 +18,11 @@ classdef HansFinger < RobotBaseClass
 
         %% Create the robot model
         function CreateModel(self)       
-            % link(1) = Link('d',0,'a',0.047,'alpha',0,'qlim',deg2rad([-10 25]),'offset',deg2rad(30)); 
-            % link(2) = Link('d',0,'a',0.04,'alpha',0,'qlim',deg2rad([0 0.01]),'offset',deg2rad(30));
-
-            % link(1) = Link('theta', 0, 'a', 0, 'alpha', 0, 'qlim', [0.1 0.5], 'offset', deg2rad(60), 'prismatic', 1);
-            link(1) = Link([0     0       0       [0.1 0.5]    1]); % PRISMATIC Link
+            link(1) = Link('theta', 0, 'a', 0, 'alpha', 0, 'qlim', [0.1 0.5], 'prismatic');
+            % link(1) = Link([0     0       0       [0.1 0.5]    1]); % PRISMATIC Link
 
             self.model = SerialLink(link,'name',self.name);
             self.model.plotopt = {'noshadow','noarrow','noshading','nowrist','nojaxes'};
-        end    
+        end        
     end
 end
